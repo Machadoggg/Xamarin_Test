6ce223e868ff1f9f49e42c1e263fb80224b9c2bc
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-09 12:31:40 -0500

    Bindin login with viewModel

---
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin_Test.ViewModels;
+
+namespace Xamarin_Test.Infrastructure
+{
+
+    
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+                Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
