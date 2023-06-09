684b7e240b8476300506d4f2e891375c780bcb57
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-09 02:03:29 -0500

    Second Commit

--- Xamarin_Test/Xamarin_Test/Xamarin_Test/App.xaml.cs
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test/App.xaml.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿
 
 namespace Xamarin_Test
 {
+    using Xamarin.Forms;
+    using Xamarin_Test.Views;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
