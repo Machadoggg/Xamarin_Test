ea4a7927557f11b2232011fd9e96e3cae16cf993
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-09 23:33:19 -0500

    Add model customers

---
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test/Models/Customers.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Test.Models
+{
+    public class Customers
+    {
+        [JsonProperty(PropertyName = "codigo")]
+        public int Codigo { get; set; }
+
+
+        [JsonProperty(PropertyName = "tipoDocumento")]
+        public string TipoDocumento { get; set; }
+
+
+        [JsonProperty(PropertyName = "numeroDocumento")]
+        public long NumeroDocumento { get; set; }
+
+
+        [JsonProperty(PropertyName = "nombres")]
+        public string Nombres { get; set; }
+
+
+        [JsonProperty(PropertyName = "apellido_1")]
+        public string Apellido_1 { get; set; }
+
+
+        [JsonProperty(PropertyName = "apellido_2")]
+        public string Apellido_2 { get; set; }
+
+
+        [JsonProperty(PropertyName = "genero")]
+        public string Genero { get; set; }
+
+
+        [JsonProperty(PropertyName = "fechaNacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+
+
+        [JsonProperty(PropertyName = "direcciones")]
+        public string Direcciones { get; set; }
+
+
+        [JsonProperty(PropertyName = "telefonos")]
+        public string Telefonos { get; set; }
+
+
+        [JsonProperty(PropertyName = "emails")]
+        public string Emails { get; set; }
+    }
+}
