52a9a1ccadd039f5e314104f7c0ec1ed917eef19
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-09 22:13:20 -0500

    Login View Model with BaseViewModel

--- Xamarin_Test/Xamarin_Test/Xamarin_Test/ViewModels/LoginViewModel.cs
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test/ViewModels/LoginViewModel.cs
@@ -1,28 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace Xamarin_Test.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        //#region Events, como herada de la BaseViewModel ya no se necesita el evento
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //#endregion
+
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
         public bool IsRemember { get; set; }
+        public bool IsEnabled 
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
-            this.IsRemember = true;  
+            this.IsRemember = true;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        public Command LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "machadoggg@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "OK",
+                    "Login OK!",
+                    "Accept");
+            return;
+        }
         #endregion
     }
 }
