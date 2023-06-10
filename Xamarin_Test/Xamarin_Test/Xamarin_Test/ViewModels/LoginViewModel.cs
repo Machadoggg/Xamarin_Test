a2575002bcac66901a5bb6330d11dd7f80ad82de
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-09 19:08:20 -0500

    Binding Login

---
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin_Test.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemember { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;  
+        }
+        #endregion
+
+        #region Commands
+        public Command LoginCommand { get; set; }
+        #endregion
+    }
+}
