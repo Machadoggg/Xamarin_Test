7dd5e51dc7630f7bc015d12b27a01676774b7a79
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-06-12 16:49:42 -0500

    Add icon to app

--- Xamarin_Test/Xamarin_Test/Xamarin_Test.Android/MainActivity.cs
+++ Xamarin_Test/Xamarin_Test/Xamarin_Test.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Xamarin_Test.Droid
 {
-    [Activity(Label = "Xamarin_Test", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Xamarin_Test", Icon = "@mipmap/ico_cfa", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
